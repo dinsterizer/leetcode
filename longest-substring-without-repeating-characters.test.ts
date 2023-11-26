77e17a03d6a7aa9aac4da01f4a045aa2b5cbd1c7
Dinh · divn.dev <me@divn.dev>
2023-11-26 19:58:04 +0700

    longest-substring-without-repeating-characters

--- longest-substring-without-repeating-characters.test.ts
+++ longest-substring-without-repeating-characters.test.ts
@@ -1,5 +1,10 @@
 import { expect, test } from 'bun:test';
+import { lengthOfLongestSubstring } from './longest-substring-without-repeating-characters';
 
-test('2 + 2', () => {
-    expect(2 + 2).toBe(4);
+test('', () => {
+    expect(lengthOfLongestSubstring('abcabcbb')).toBe(3);
+    expect(lengthOfLongestSubstring('bbbbb')).toBe(1);
+    expect(lengthOfLongestSubstring('pwwkew')).toBe(3);
+    expect(lengthOfLongestSubstring('dvdf')).toBe(3);
+    expect(lengthOfLongestSubstring('aabaab!bb')).toBe(3);
 });
--- longest-substring-without-repeating-characters.ts
+++ longest-substring-without-repeating-characters.ts
@@ -1,1 +1,16 @@
-function lengthOfLongestSubstring(s: string): number {}
+export function lengthOfLongestSubstring(s: string): number {
+    let length = 0;
+    let stringArr: string[] = [];
+
+    [...s].forEach((char) => {
+        const i = stringArr.findIndex((c) => c === char);
+        if (i !== -1) {
+            length = length < stringArr.length ? stringArr.length : length;
+            stringArr = stringArr.slice(i + 1);
+        }
+
+        stringArr.push(char);
+    });
+
+    return length < stringArr.length ? stringArr.length : length;
+}
